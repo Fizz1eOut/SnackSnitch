3ef2593a5ad8eff47cede28b91648905b7a19c34
FizzleOut <fizzleout1337@gmail.com>
2025-04-27 13:32:55 +0200

    feat(AppLink): added styles for the `AppLink` component

---
+++ src/components/Base/AppLink.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div class="link line">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+  .link {
+    font-size: 16px;
+    font-weight: 400;
+    color: var(--color-white);
+    font-family: "Inter", sans-serif;
+  }
+</style>
