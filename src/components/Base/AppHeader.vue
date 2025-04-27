bff372b20bbc0bc687e6c40199cecfc672539ba4
FizzleOut <fizzleout1337@gmail.com>
2025-04-27 20:34:36 +0200

    feat(app-header): added header component with adaptive navigation
    - Created AppHeader component
    - Used useMediaQuery to detect mobile device
    - Added AppNav and AppNavMobile components with switching by screen width
    - Set up basic header structure and styles

---
+++ src/components/Base/AppHeader.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  import AppContainer from '@/components/Base/AppContainer.vue';
+  import AppNav from '@/components/Base/AppNav.vue';
+  import AppNavMobile from '@/components/Base/AppNavMobile.vue';
+  import { useMediaQuery } from '@/composables/useMediaQuery';
+
+  const { isMobile } = useMediaQuery('(max-width: 768px)');
+</script>
+<template>
+  <div class="header">
+    <app-container>
+      <div class="header__body">
+        <div class="header__img">
+          <router-link to="/">
+            <img src="../../assets/logo.png" alt="Logo">
+          </router-link>
+        </div>
+        <app-nav v-if="!isMobile" />
+        <app-nav-mobile 
+          v-if="isMobile"
+        />
+      </div>
+    </app-container>
+  </div>
+</template>
+
+<style scoped>
+  .header__body {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10px;
+  }
+  .header__actions {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+  .header__img {
+    position: relative;
+    z-index: 11;
+  }
+  .header__img img {
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+  }
+  @media (max-width: 768px) {
+    .header__wrapper {
+      max-width: 100%;
+    }
+  }
+</style>
