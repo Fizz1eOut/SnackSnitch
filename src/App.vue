0961019962ff1a28d37296df4975debc25bbf0b8
FizzleOut <fizzleout1337@gmail.com>
2025-05-02 19:19:52 +0200

    feat(app-vue): hide AppHeader if user profile is empty
    - Connected Pinia user store and isUserRegistered computed property
    - AppHeader component is only shown if user.profile is present
    - Improved header display logic depending on user state

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+  import { computed } from 'vue';
   import { RouterView } from 'vue-router';
+  import { useUserStore } from '@/stores/user'; 
   import AppContainer from '@/components/Base/AppContainer.vue';
   import AppHeader from '@/components/Base/AppHeader.vue';
+
+  const userStore = useUserStore();
+  const isUserRegistered = computed(() => userStore.currentUser?.profile);
 </script>
 
 <template>
-  <app-header />
+  <app-header v-if="isUserRegistered" />
   <main class="main">
     <app-container>
       <router-view />
