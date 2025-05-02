03664beb73449dbe6919c7bef477fd3e52ec2a36
FizzleOut <fizzleout1337@gmail.com>
2025-05-02 19:18:04 +0200

    feat(types): added User and UserProfile interfaces
    - User contains username, password and optional profile field
    - UserProfile includes height, weight, age, gender, goal and activity level parameters
    - Used in storage and working with IndexedDB

---
+++ src/interface/user.interface.ts
@@ -0,0 +1,14 @@
+export interface User {
+  username: string;
+  password: string;
+  profile?: UserProfile;
+};
+
+export interface UserProfile {
+  height: number;
+  weight: number;
+  age: number;
+  gender: 'male' | 'female';
+  goal: 'lose' | 'maintain' | 'gain';
+  activity: 'low' | 'medium' | 'high';
+}
